c6283ecf9c678939401b61516f71f00f9bb18ff5
cristianiniguez <felizpc1@gmail.com>
2021-12-26 22:01:54 -0400

    extender la accesibilidad usando javascript: validando formularios

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.onload = () => {
   document.querySelector('.arrow-right').addEventListener('click', clickRight);
   document.querySelector('.arrow-left').addEventListener('click', clickLeft);
-  document.querySelector('.send-button').addEventListener('click', showNotification);
+  document.querySelector('.send-button').addEventListener('click', (e) => validateForm(e));
   document.querySelectorAll('.project').forEach((element) => {
     element.addEventListener('click', (e) => openModal(e));
   });
@@ -69,8 +69,23 @@ function clickLeft() {
   }
 }
 
+/** Validar el formulario antes de mostrar la notificación */
+function validateForm(e) {
+  e.preventDefault();
+  const nameField = document.getElementById('name');
+
+  if (nameField.value === '') {
+    document.getElementById('name-error').innerHTML =
+      'Para enviar el formulario, se necesita un nombre';
+  } else {
+    showNotification();
+  }
+}
+
 /** Esta funcion se llama cuando la persona hace click en el boton de enviar del formulario de contacto */
 function showNotification() {
+  document.getElementById('name-error').innerHTML = '';
+  document.querySelector('.form-container').reset();
   document.querySelector('.notification').style.display = 'flex';
   setTimeout(function () {
     document.querySelector('.notification').style.display = 'none';
