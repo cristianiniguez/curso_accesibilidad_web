d81379e7db9f5c7cfbf07547a942dbb3d5d2f180
cristianiniguez <felizpc1@gmail.com>
2021-12-26 21:34:58 -0400

    extender la accesibilidad usando javascript: mejorando la accesibilidad con teclado de nuestro modal

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ window.onload = () => {
     element.addEventListener('click', (e) => openModal(e));
   });
   document.body.addEventListener('click', (e) => closeModal(e));
+  document.body.addEventListener('keyup', listenForEsc);
 };
 
 /** Esta funcion se llama cuando la persona hace click en la fecha derecha del carousel para navegar a la derecha */
@@ -79,6 +80,7 @@ function showNotification() {
 /** Esta funcion se llama cuando la persona hace click en cualquier porjecto del carousel */
 function openModal(e) {
   document.querySelector('.modal-container').style.display = 'flex';
+  document.getElementById('modal-header').focus();
 }
 
 /** Esta funcion se llama para cerrar el modal */
@@ -88,5 +90,12 @@ function closeModal(e) {
     return;
   } else {
     document.querySelector('.modal-container').style.display = 'none';
+  }
+}
+
+/** Esta escucha por la clave esc para cerrar el modal */
+function listenForEsc(e) {
+  if (e.keyCode === 27) {
+    closeModal(e);
   }
 }
